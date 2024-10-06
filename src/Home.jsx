e8aaa91b54909ca40491ccda18c1b669444edd0e
Eric-Okyere <ericokyere018@gmail.com>
2024-10-06 05:17:38 +0000

    Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,26 @@ const Home = () => {
   return (
     <div>
         <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
+        <h1 className="pt-40 flex justify-center">Home</h1>
        
         </div>
   )
